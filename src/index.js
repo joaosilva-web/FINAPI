f78f624cd2e94ede6821567de67893dabb593fc0
João Gustavo <joaogustavosilva2001@gmail.com>
2021-11-12 16:06:04 -0300

    description,

--- src/index.js
+++ src/index.js
@@ -23,6 +23,17 @@ function verifyIfExistsAccountCFP(req, res, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit"){
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
 
 app.post('/account', (req, res) => {
     const {cpf, name} = req.body;
@@ -63,6 +74,27 @@ app.post('/deposit', verifyIfExistsAccountCFP, (req, res) => {
 
     customer.statement.push(statementOperation);
 
+
+    return res.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCFP, (req, res) => {
+    const { amount } = req.body;
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) {
+        return res.status(400).json({error: "Insufficient funds"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit',
+    };
+
+    customer.statement.push(statementOperation);
 
     return res.status(201).send();
 });
